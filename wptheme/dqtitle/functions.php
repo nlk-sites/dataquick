44f9dbcdd756656f09fe69a0d623ccedcde11686
Tim Spinks <tim@ninthlink.com>
2013-04-23 20:35:51 +0000

    remove url encoding

--- wptheme/dqtitle/functions.php
+++ wptheme/dqtitle/functions.php
@@ -261,7 +261,7 @@ function set_post_content( $entry, $form ) {
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
         //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($xml));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "$xml");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLINFO_HEADER_OUT, true);
         $output = curl_exec($ch);
